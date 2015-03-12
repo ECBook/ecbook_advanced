69f934fe8608e1f6f8d0774228d24bc3dd74ef75
maylas <maylas@spengergasse.at>
2015-03-12 22:33:42 +0100

    added guest-info

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,22 +1,35 @@
 <?php
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
+use common\models;
 ?>
 <div class="site-index">
 
+<p> Willkommen, 
+<br>
+<?php
+ if (Yii::$app->user->getIsGuest())
+                        {
+						print("Bitte anmelden, Sie sind Gast");
+						}
+						
+?>
+
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+		
+        <!--<h1>Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+		-->
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <!--<h2>Heading</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -44,6 +57,7 @@ $this->title = 'My Yii Application';
                     fugiat nulla pariatur.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+				-->
             </div>
         </div>
 
